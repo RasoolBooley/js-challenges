291d51cbd40f1a02a0ae1612bf8187a82d794089
RasoolBooley <45028873+RasoolBooley@users.noreply.github.com>
2019-02-20 12:51:45 +0200

    challenge one

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # js-challenges
+
+JavaScript Challenges for Life Choices
+
+## Technologies used
+
+- VScode
+- Gitbash
+- Github
+
+### Languages used
+
+- HTML
+- JavaScript
+
+### Changelog
+
+2/20/2019
+
+- Finished Challenge one
---
+++ challenge1/js/main.js
@@ -0,0 +1,40 @@
+//  array
+
+let fruit = ["apple", "grapes", "peach"];
+
+console.log(fruit);
+
+// object
+
+let guitar = {
+    brand:"Jackson", model:"RR500", color:"white"
+}
+
+console.log(guitar);
+
+// string
+
+console.log("Check out muh strings brah");
+
+// number
+
+console.log(4*6);
+
+// boolean
+
+console.log(Boolean(1))
+
+// null
+
+console.log(null)
+
+//  undefined
+
+console.log(undefined);
+
+// date
+
+time = Date();
+
+console.log(time);
+
